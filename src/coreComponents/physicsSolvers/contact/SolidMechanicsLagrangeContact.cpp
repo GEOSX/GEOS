5d0065a119346fc5bb98a57b397debff289915e1
Pavel Tomin <paveltomin@users.noreply.github.com>
2024-11-22 16:02:47 -0600

    Update SolidMechanicsLagrangeContact.cpp

--- src/coreComponents/physicsSolvers/contact/SolidMechanicsLagrangeContact.cpp
+++ src/coreComponents/physicsSolvers/contact/SolidMechanicsLagrangeContact.cpp
@@ -2262,7 +2262,7 @@ bool SolidMechanicsLagrangeContact::updateConfiguration( DomainPartition & domai
             integer const originalFractureState = fractureState[kfe];
             if( originalFractureState == FractureState::Open )
             {
-              if( dispJump[kfe][0] <= normalDisplacementTolerance[kfe] )
+              if( dispJump[kfe][0] <= -normalDisplacementTolerance[kfe] )
               {
                 fractureState[kfe] = FractureState::Stick;
                 if( getLogLevel() >= 10 )
