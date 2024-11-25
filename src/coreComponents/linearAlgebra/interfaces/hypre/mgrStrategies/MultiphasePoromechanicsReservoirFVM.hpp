8fbb5d52c449110b2277ba2ca217f174e1399a3c
Victor A. P. Magri <paludettomag1@llnl.gov>
2024-11-25 18:11:30 -0500

    Use injection as restriction and prolongation in first level

--- src/coreComponents/linearAlgebra/interfaces/hypre/mgrStrategies/MultiphasePoromechanicsReservoirFVM.hpp
+++ src/coreComponents/linearAlgebra/interfaces/hypre/mgrStrategies/MultiphasePoromechanicsReservoirFVM.hpp
@@ -86,8 +86,8 @@ public:
     // Level 0
     m_levelFRelaxType[0]          = MGRFRelaxationType::amgVCycle;
     m_levelFRelaxIters[0]         = 1;
-    m_levelInterpType[0]          = MGRInterpolationType::jacobi;
-    m_levelRestrictType[0]        = MGRRestrictionType::jacobi; //MGRRestrictionType::injection;
+    m_levelInterpType[0]          = MGRInterpolationType::injection;
+    m_levelRestrictType[0]        = MGRRestrictionType::injection; //MGRRestrictionType::jacobi;
     m_levelCoarseGridMethod[0]    = (MGRCoarseGridMethod) 5; //MGRCoarseGridMethod::nonGalerkin;
     m_levelGlobalSmootherType[0]  = MGRGlobalSmootherType::none;
 
