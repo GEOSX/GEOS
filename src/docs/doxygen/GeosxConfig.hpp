9b5a13240fec038da479024a384b7b1f0c838d30
Pavel Tomin <ptls@PW0BMYGN>
2024-11-27 15:28:55 -0600

    revert

--- src/docs/doxygen/GeosxConfig.hpp
+++ src/docs/doxygen/GeosxConfig.hpp
@@ -135,7 +135,7 @@
 /* #undef chai_VERSION */
 
 /// Version information for adiak
-/* #undef adiak_VERSION */
+#define adiak_VERSION ..
 
 /// Version information for caliper
 #define caliper_VERSION 2.8.0
