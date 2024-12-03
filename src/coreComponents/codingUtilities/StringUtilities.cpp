722a88a9de673e6f862daf62210002edec095144
Pavel Tomin <paveltomin@users.noreply.github.com>
2024-12-03 14:47:18 -0600

    Update src/coreComponents/codingUtilities/StringUtilities.cpp
    Co-authored-by: William R Tobin <4522899+wrtobin@users.noreply.github.com>

--- src/coreComponents/codingUtilities/StringUtilities.cpp
+++ src/coreComponents/codingUtilities/StringUtilities.cpp
@@ -74,7 +74,9 @@ template< typename T >
 string toMetricPrefixString( T const & value )
 {
   if( std::fpclassify( value ) == FP_ZERO )
+  {
     return " 0.0  ";
+  }
 
   // These are the metric prefixes corrosponding to kilo, mega, giga...etc.
   char const prefixes[12] = { 'f', 'p', 'n', 'u', 'm', ' ', 'K', 'M', 'G', 'T', 'P', 'E'};
