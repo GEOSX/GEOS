25ddb6a53c9483ba7b97b8d91ff72e26dad59e70
Ryan Aronson <raronson0031@gmail.com>
2024-12-05 12:00:05 -0800

    Update SolidMechanicsStateReset.cpp

--- src/coreComponents/physicsSolvers/solidMechanics/SolidMechanicsStateReset.cpp
+++ src/coreComponents/physicsSolvers/solidMechanics/SolidMechanicsStateReset.cpp
@@ -99,7 +99,7 @@ bool SolidMechanicsStateReset::execute( real64 const time_n,
       nodeManager.getField< solidMechanics::totalDisplacement >().zero();
       nodeManager.getField< solidMechanics::incrementalDisplacement >().zero();
 
-      ElementRegionManager & elemManager = meshLevel.getElemManager();
+      ElementRegionManager & elemManager = mesh.getElemManager();
       elemManager.forElementSubRegions< CellElementSubRegion >( regionNames,
                                                               [&]( localIndex const,
                                                                    ElementSubRegionBase & subRegion )
