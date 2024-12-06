a929eb8cb937057e1152588ef0ad553adc0173c3
Matteo Cusini <matteo.cusini@gmail.com>
2024-12-06 13:13:40 -0800

    fortran compiler on LC

--- host-configs/LLNL/lassen-clang-13-cuda-12.cmake
+++ host-configs/LLNL/lassen-clang-13-cuda-12.cmake
@@ -1,12 +1,5 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../../src/coreComponents/LvArray/host-configs/LLNL/lassen-clang-13-cuda-12.cmake)
 
-# Fortran
-set(CMAKE_Fortran_COMPILER /usr/tce/packages/gcc/gcc-8.3.1/bin/gfortran  CACHE PATH "")
-set(CMAKE_Fortran_FLAGS_RELEASE "-O3 -DNDEBUG -mtune=power9" CACHE STRING "")
-set(FORTRAN_MANGLE_NO_UNDERSCORE ON CACHE BOOL "")
-set(OpenMP_Fortran_FLAGS "-fopenmp" CACHE STRING "")
-set(OpenMP_Fortran_LIB_NAMES "" CACHE STRING "")
-
 # MPI
 set(MPI_HOME /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-clang-13.0.1-gcc-8.3.1/ CACHE PATH "")
 set(MPI_Fortran_COMPILER /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-gcc-8.3.1/bin/mpifort CACHE PATH "")
