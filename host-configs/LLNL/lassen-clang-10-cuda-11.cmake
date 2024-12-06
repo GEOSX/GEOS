9cffe8e05dedcf7e5823960dc681532a6a429ca6
Matteo Cusini <matteo.cusini@gmail.com>
2024-12-06 13:58:34 -0800

    try different fortran compilers.

--- host-configs/LLNL/lassen-clang-10-cuda-11.cmake
+++ host-configs/LLNL/lassen-clang-10-cuda-11.cmake
@@ -2,7 +2,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/../../src/coreComponents/LvArray/host-configs/
 
 # MPI
 set(MPI_HOME /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-clang-10.0.1-gcc-8.3.1 CACHE PATH "")
-set(MPI_Fortran_COMPILER /usr/tce/packages/spectrum-mpi/spectrum-mpi-rolling-release-xl-2022.08.19-cuda-11.8.0/bin/mpifort CACHE PATH "")
+set(MPI_Fortran_COMPILER spectrum-mpi-rolling-release-xl-2023.06.28-cuda-11.8.0/bin/mpifort CACHE PATH "")
 
 include(${CMAKE_CURRENT_LIST_DIR}/lassen-base.cmake)
 
