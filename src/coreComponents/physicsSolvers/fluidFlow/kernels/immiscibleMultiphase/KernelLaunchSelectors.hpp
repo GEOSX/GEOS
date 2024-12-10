3ddd347c31a13942031952ada2f5c64aa3b7a7be
DENEL Bertrand <bertrand.denel@total.com>
2024-12-10 12:29:04 -0600

    Changed Total S.A.

--- src/coreComponents/physicsSolvers/fluidFlow/kernels/immiscibleMultiphase/KernelLaunchSelectors.hpp
+++ src/coreComponents/physicsSolvers/fluidFlow/kernels/immiscibleMultiphase/KernelLaunchSelectors.hpp
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: LGPL-2.1-only
  *
  * Copyright (c) 2016-2024 Lawrence Livermore National Security LLC
- * Copyright (c) 2018-2024 Total, S.A
+ * Copyright (c) 2018-2024 TotalEnergies
  * Copyright (c) 2018-2024 The Board of Trustees of the Leland Stanford Junior University
  * Copyright (c) 2023-2024 Chevron
  * Copyright (c) 2019-     GEOS/GEOSX Contributors
@@ -20,10 +20,6 @@
 #ifndef GEOS_PHYSICSSOLVERS_FLUIDFLOW_IMMISCIBLE_KERNELLAUNCHSELECTOR_HPP
 #define GEOS_PHYSICSSOLVERS_FLUIDFLOW_IMMISCIBLE_KERNELLAUNCHSELECTOR_HPP
 
-//#include "codingUtilities/Utilities.hpp"
-//#include "common/DataLayouts.hpp"
-//#include "common/DataTypes.hpp"
-//#include "common/GEOS_RAJA_Interface.hpp"
 
 namespace geos
 {
