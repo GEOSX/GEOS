dc805b1499023fb1d4df00fb64f964693df9af2d
Victor A. P. Magri <paludettomag1@llnl.gov>
2024-12-18 22:42:21 -0800

    Link flag workaround

--- host-configs/LLNL/tioga-cce-18-rocm-6.2.1.cmake
+++ host-configs/LLNL/tioga-cce-18-rocm-6.2.1.cmake
@@ -3,3 +3,7 @@ include(${CMAKE_CURRENT_LIST_DIR}/tioga-base.cmake)
 
 # MPI
 set(MPI_HOME /opt/cray/pe/mpich/8.1.31/ofi/crayclang/18.0 CACHE PATH "")
+
+# Workaround for linking errors involving missing explicit instantiations of certain functions.
+# TODO: we should add the missing explicit instantiations
+set( CMAKE_CXX_LINK_FLAGS "${CMAKE_CXX_LINK_FLAGS} -Wl,--allow-shlib-undefined" CACHE STRING "" FORCE )
