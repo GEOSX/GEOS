17c39ef13610236823631bea892fccfbeee27b7d
arng40 <arnaud.dudes@capgemini.com>
2025-01-22 11:33:16 +0100

    code style

--- src/coreComponents/physicsSolvers/multiphysics/HydrofractureSolver.cpp
+++ src/coreComponents/physicsSolvers/multiphysics/HydrofractureSolver.cpp
@@ -379,17 +379,17 @@ void HydrofractureSolver< POROMECHANICS_SOLVER >::updateHydraulicApertureAndFrac
   GEOS_LOG_LEVEL_INFO_RANK_0( logInfo::Solution,
                               GEOS_FMT( "        {}: Max aperture change: {} m, max hydraulic aperture change: {} m",
                                         this->getName(),
-                                        fmt::format( "{:.{}e}", maxApertureChange, 6 ), 
+                                        fmt::format( "{:.{}e}", maxApertureChange, 6 ),
                                         fmt::format( "{:.{}e}", maxHydraulicApertureChange, 6 ) ) );
   GEOS_LOG_LEVEL_INFO_RANK_0( logInfo::Solution,
                               GEOS_FMT( "        {}: Min aperture: {} m, max aperture: {} m",
                                         this->getName(),
-                                        fmt::format( "{:.{}e}", minAperture, 6 ), 
+                                        fmt::format( "{:.{}e}", minAperture, 6 ),
                                         fmt::format( "{:.{}e}", maxAperture, 6 ) ) );
   GEOS_LOG_LEVEL_INFO_RANK_0( logInfo::Solution,
                               GEOS_FMT( "        {}: Min hydraulic aperture: {} m, max hydraulic aperture: {} m",
                                         this->getName(),
-                                        fmt::format( "{:.{}e}", minHydraulicAperture, 6 ), 
+                                        fmt::format( "{:.{}e}", minHydraulicAperture, 6 ),
                                         fmt::format( "{:.{}e}", maxHydraulicAperture, 6 ) ) );
 }
 template< typename POROMECHANICS_SOLVER >
